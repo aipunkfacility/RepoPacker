88331216319c86f28691de9d4adc5d41573017b3
aipunkfacility <ai.punk.facility@gmail.com>
2026-01-10 00:37:07 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,17 @@
+export const TEXT_EXTENSIONS = new Set([
+    'js', 'jsx', 'ts', 'tsx', 'json', 'css', 'scss', 'html', 'md', 'txt', 
+    'py', 'java', 'c', 'cpp', 'h', 'cs', 'go', 'rs', 'php', 'rb', 'sh', 
+    'yaml', 'yml', 'xml', 'sql', 'gitignore', 'env', 'dockerfile', 'toml', 'ini', 'conf'
+]);
 
+export const IGNORE_DIRS = new Set([
+    '.git', 'node_modules', 'dist', 'build', 'coverage', '.idea', '.vscode', '__pycache__', 'venv', 'bin', 'obj'
+]);
+
+export const IGNORE_FILES = new Set([
+    'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', '.DS_Store', 'thumbs.db'
+]);
+
+export const ALLOWED_FILENAMES = new Set([
+    'dockerfile', 'makefile', 'license', 'readme', 'changelog'
+]);
